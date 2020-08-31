3d3dbc457b8fa04eadc0c221e6c8575a223b4406
Noah Jutz <noah.jutz@gmail.com>
2020-08-31 11:11:10 +0200

    Add basic layout
    * TopBar
    * FAB

--- app/src/main/java/com/example/exercisenotes/MainActivity.kt
+++ app/src/main/java/com/example/exercisenotes/MainActivity.kt
@@ -2,20 +2,58 @@ package com.example.exercisenotes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Description
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme(colors = if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-                Text("Hello World!")
+                MainScreen()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("ExerciseNotes") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {},
+                        icon = { Icon(Icons.Filled.Description) }
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                icon = { Icon(Icons.Filled.Add) }
+            )
+        },
+        bodyContent = {
+
+        }
+    )
+}
+
+@Composable
+@Preview
+fun MainScreenPreview() {
+    MaterialTheme {
+        MainScreen()
     }
 }
