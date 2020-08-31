8f1a625223da2de2860f6f8dd59ea001aa9a6253
Noah Jutz <noah.jutz@gmail.com>
2020-08-31 11:14:31 +0200

    Add ExercisesList Composable

--- app/src/main/java/com/example/exercisenotes/MainActivity.kt
+++ app/src/main/java/com/example/exercisenotes/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 
@@ -45,7 +48,7 @@ fun MainScreen() {
             )
         },
         bodyContent = {
-
+            ExercisesList()
         }
     )
 }
@@ -55,5 +58,16 @@ fun MainScreen() {
 fun MainScreenPreview() {
     MaterialTheme {
         MainScreen()
+    }
+}
+
+@Composable
+fun ExercisesList() {
+    val mockExercises = listOf("Push Up", "Lunges", "Chest Press")
+    LazyColumnFor(items = mockExercises) { exercise ->
+        ListItem(
+            text = { Text(exercise) },
+            modifier = Modifier.clickable(onClick = {})
+        )
     }
 }
