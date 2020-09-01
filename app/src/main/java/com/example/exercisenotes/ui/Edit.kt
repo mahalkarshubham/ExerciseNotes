78356b64ef32701f95d88c21e7ed03bda8016c6d
Noah Jutz <noah.jutz@gmail.com>
2020-09-01 11:37:19 +0200

    Add basic layout for Edit

--- app/src/main/java/com/example/exercisenotes/ui/Edit.kt
+++ app/src/main/java/com/example/exercisenotes/ui/Edit.kt
@@ -1,14 +1,17 @@
 package com.example.exercisenotes.ui
 
+import androidx.compose.foundation.Box
 import androidx.compose.foundation.Icon
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.exercisenotes.data.Exercise
 
@@ -30,9 +33,27 @@ fun EditExercise(
             )
         },
         bodyContent = {
-            Text(exercise.name)
+            EditExerciseContent()
         }
     )
+}
+
+@Composable
+fun EditExerciseContent() {
+    ScrollableColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+        TextField(
+            label = { Text("Name") },
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp, top = 16.dp)
+        )
+        TextField(
+            label = {Text("Description")},
+            value = "",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+        )
+    }
 }
 
 @Composable
