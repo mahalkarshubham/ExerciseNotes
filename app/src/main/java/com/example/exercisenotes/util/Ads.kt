78f86c017ce95c2299b90cd560bb1c4c39be97f6
Noah Jutz <noah.jutz@gmail.com>
2020-09-02 12:22:20 +0200

    Add Interstitial ad id

--- app/src/main/java/com/example/exercisenotes/util/Ads.kt
+++ app/src/main/java/com/example/exercisenotes/util/Ads.kt
@@ -6,4 +6,7 @@ object Ads {
     const val exercisesBannerId = "ca-app-pub-7450355346929746/2632503867"
     const val editExerciseBannerId = "ca-app-pub-7450355346929746/2140483614"
     const val testBannerId = "ca-app-pub-3940256099942544/6300978111"
+
+    const val myInterstitialId = "ca-app-pub-7450355346929746/1182625165"
+    const val testInterstitialId = "ca-app-pub-3940256099942544/1033173712"
 }
