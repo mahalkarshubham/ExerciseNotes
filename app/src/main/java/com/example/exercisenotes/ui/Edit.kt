61e61c26c7abadfc980a3c04b0491c23854e5f02
Noah Jutz <noah.jutz@gmail.com>
2020-09-02 12:19:03 +0200

    Add BannerAd to Edit screen

--- app/src/main/java/com/example/exercisenotes/ui/Edit.kt
+++ app/src/main/java/com/example/exercisenotes/ui/Edit.kt
@@ -3,6 +3,7 @@ package com.example.exercisenotes.ui
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
@@ -17,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.exercisenotes.data.Exercise
 import com.example.exercisenotes.data.ExerciseDao
+import com.example.exercisenotes.util.Ads
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
@@ -77,25 +79,28 @@ fun EditExerciseContent(
 ) {
     val name = remember { mutableStateOf(exercise.name) }
     val desc = remember { mutableStateOf(exercise.description) }
-    ScrollableColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
-        TextField(
-            label = { Text("Name") },
-            value = name.value,
-            onValueChange = {
-                name.value = it
-                setExerciseName(name.value)
-            },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp, top = 16.dp)
-        )
-        TextField(
-            label = { Text("Description") },
-            value = desc.value,
-            onValueChange = {
-                desc.value = it
-                setExerciseDesc(desc.value)
-            },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-        )
+    Column {
+        ScrollableColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp).weight(1f)) {
+            TextField(
+                label = { Text("Name") },
+                value = name.value,
+                onValueChange = {
+                    name.value = it
+                    setExerciseName(name.value)
+                },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp, top = 16.dp)
+            )
+            TextField(
+                label = { Text("Description") },
+                value = desc.value,
+                onValueChange = {
+                    desc.value = it
+                    setExerciseDesc(desc.value)
+                },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
+        }
+        BannerAd(adId = Ads.editExerciseBannerId)
     }
 }
 
--- app/src/main/java/com/example/exercisenotes/util/Ads.kt
+++ app/src/main/java/com/example/exercisenotes/util/Ads.kt
@@ -4,5 +4,6 @@ object Ads {
     const val appId = "ca-app-pub-7450355346929746~1347342351"
 
     const val exercisesBannerId = "ca-app-pub-7450355346929746/2632503867"
+    const val editExerciseBannerId = "ca-app-pub-7450355346929746/2140483614"
     const val testBannerId = "ca-app-pub-3940256099942544/6300978111"
 }
